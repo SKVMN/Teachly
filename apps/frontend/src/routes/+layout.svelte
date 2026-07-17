a59f3d7105b08c294c40626aea72ccc08581e330
Maksim Skvortsov <skvortsov.m.n@yandex.ru>
2026-07-18 00:34:51 +0300

    base layout

--- apps/frontend/src/routes/+layout.svelte
+++ apps/frontend/src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
-  import './layout.css';
   import favicon from '$lib/assets/favicon.svg';
+  import './layout.css';
 
   let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<div class="flex h-full w-full flex-col">
+  <div class="shrink-0 grow-0 basis-12 content-center bg-amber-200 p-3">sdfasdfaf</div>
+  <div class="main-container flex-auto shrink grow basis-full bg-amber-900 p-3"></div>
+
+  {@render children()}
+</div>
